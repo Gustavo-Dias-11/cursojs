0b3e328babb258bfea4a1f772db87bfdff687852
Gustavo-Dias-11 <gustavo.dias.raymundi@gmail.com>
2025-03-14 14:22:40 -0300

    Initial commit

---
+++ aula 11/ex010.js
@@ -0,0 +1,9 @@
+var kilos = 89
+console.log(`seu peso é de ${kilos}`)
+if (kilos > 90){
+    console.log('voce é um obeso')
+}
+else
+{
+    console.log('voce é normal')
+}
---
+++ aula 12/ex011.js
@@ -0,0 +1,19 @@
+var idade = 67
+if(idade < 16){
+    console.log('Não vota')
+} 
+else{
+    if(idade < 18 )
+    console.log('voto opcional')
+    else{
+        if(idade > 18 && idade < 65)
+        console.log('voto obrigatorio')
+    else{
+        if(idade > 65){
+        console.log('voto opcional')
+        }
+    
+    }
+    
+    }
+}
